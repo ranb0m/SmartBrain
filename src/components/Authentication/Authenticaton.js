a2b64db1754d8881715feeae88e4580ae0d8d1a1
Cristian <cristiansolomon100@gmail.com>
2023-12-07 19:21:29 -0500

    final changes to the front-end before starting back-end

--- src/components/Authentication/Authenticaton.js
+++ src/components/Authentication/Authenticaton.js
@@ -1,19 +1,18 @@
 import React from "react";
-import { useState } from "react";
 
 export default function Authentication({ onRouteChange, route }) {
   return (
-    <article class="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center shadow-5">
+    <article className="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center shadow-5">
       <main className="pa4 black-80">
-        <form className="measure">
+        <div className="measure">
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
             <legend className="f4 fw6 ph0 mh0">{route === 'sign-in' ? 'Sign In' : 'Register'}</legend>
             <div className="mt3">
-              <label className="db fw6 lh-copy f6" for="email-address">Email</label>
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
               <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
             </div>
             <div className="mv3">
-              <label className="db fw6 lh-copy f6" for="password">Password</label>
+              <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
               <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
             </div>
           </fieldset>
@@ -22,18 +21,21 @@ export default function Authentication({ onRouteChange, route }) {
                    type="submit"
                    value={route ==='sign-in' ? "Sign in" : "Register"}
                    onClick={() => {
-                    onRouteChange('home');
+                    if (route ==='sign-in')
+                    onRouteChange('home')
+                    else if (route === 'registration')
+                    onRouteChange('sign-in');
                    }} />
           </div>
           <div className="lh-copy mt3">
             {route === 'sign-in' && <a onClick={() => {
               onRouteChange('registration');
               }
-            } className="f6 link dim black db">Register
+            } className="f6 link dim black db pointer">Register
             </a>
             }
           </div>
-        </form>
+        </div>
     </main>
   </article>
 
